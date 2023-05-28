1488085437a4a29e4c5049e1318f6d4cd515ad5c
Diblans <tonyblack47@gmail.com>
2023-05-29 00:03:19 +0300

    Изменения для проектной работы

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/model/FilterContactTypeUi.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/model/FilterContactTypeUi.java
@@ -2,7 +2,10 @@ package ru.yandex.practicum.contacts.presentation.filter.model;
 
 import androidx.annotation.NonNull;
 
-public class FilterContactTypeUi {
+
+    import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
+
+    public class FilterContactTypeUi implements ListDiffInterface<FilterContactTypeUi> {
 
     private final FilterContactType contactType;
     private final boolean selected;
@@ -19,8 +22,12 @@ public class FilterContactTypeUi {
     public boolean isSelected() {
         return selected;
     }
+        @Override
+        public boolean theSameAs(FilterContactTypeUi item) {
+            return this.getContactType() == item.getContactType();
+        }
 
-    @Override
+        @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
